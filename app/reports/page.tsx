4ffa384b3feeb41c023e7122e88529606c6e0811
macfros <macfros@gmail.com>
2024-04-24 11:29:09 +0530

    ...

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -7,16 +7,16 @@ import Image from "next/image";
 
 const Reports = () => {
     return(
-<BaseLayout>
-  <div className="flex justify-center items-center h-screen">
-    <div className="text-center">
-      <PageTitle title="This feature will be live soon.." />
-      <div className="mx-auto my-auto">
-        <Image src="/working.png" alt="Working image" height={200} width={200} />
-      </div>
-    </div>
-  </div>
-</BaseLayout>
+            <BaseLayout>
+            <div className="flex justify-center items-center h-screen">
+                <div className="text-center">
+                <PageTitle title="This feature will be live soon.." />
+                <div className="mx-auto my-auto">
+                    <Image src="/working.png" alt="Working image" height={200} width={200} />
+                </div>
+                </div>
+            </div>
+            </BaseLayout>
     )
 }
 
